8bd39c7b5729beb162246c5d888bdec2ad681758
SakibFahim <sakibfahim1999@gmail.com>
2022-05-09 21:47:56 +0600

    Add a new file

---
+++ New_package_1/new_java_class_1.java
@@ -0,0 +1,27 @@
+package New_package_1;
+
+public class new_java_class_1 {
+    string name;
+    int roll;
+
+    public new_java_class_1(string name, int roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}
